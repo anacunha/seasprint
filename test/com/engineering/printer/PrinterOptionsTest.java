517a602af2e3c01f9075c9d632b13238402e9695
Ana Cunha <analza.cunha@gmail.com>
2012-12-01 15:21:14 -0500

    Included test folder

---
+++ test/com/engineering/printer/PrinterOptionsTest.java
@@ -0,0 +1,16 @@
+package com.engineering.printer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrinterOptionsTest {
+
+	private PrinterOptions printerOptions;
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
